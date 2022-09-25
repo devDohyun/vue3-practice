0802e26bdeabc1d2db39e6aab92c82a614a3a664
devDohyun <dohyun10101@gmail.com>
2022-09-25 17:29:49 +0900

    Chore: 버전 변경, config 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
-const path = require('path')
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import vue from '@vitejs/plugin-vue'
 
-function pathResolve(dir: string) {
-  return path.resolve(process.cwd(), '.', dir)
-}
-
-export default () => {
-  return {
-    resolve: {
-      alias: [
-        {
-          find: /\/@\//,
-          replacement: pathResolve('src') + '/',
-        },
-      ],
-    },
-  }
-}
+export default defineConfig({
+  plugins: [tsconfigPaths(), vue()],
+})
