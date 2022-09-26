bec7b33c893944ce7da04cd9852ed4b670847c6e
devDohyun <dohyun10101@gmail.com>
2022-09-26 21:36:17 +0900

    Feat: 간단한 문구 추가

--- src/App.vue
+++ src/App.vue
@@ -12,5 +12,22 @@ export default defineComponent({
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <p class="quote">
+      Vue 3로 만든 Todo app !<br />
+      CRUD의 기능을 localStorage와 함께 합니다.<br />
+      추가, 삭제, 수정 후 새로고침을 마음껏 해보세요 !<br />
+    </p>
+  </div>
   <Todo />
 </template>
+
+<style scoped>
+.quote {
+  display: inline-block;
+
+  padding: 10px;
+
+  border: dashed 1px black;
+}
+</style>
