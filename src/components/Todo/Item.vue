15433b95835d3d6064f856985faae826f228ef5c
devDohyun <dohyun10101@gmail.com>
2022-09-26 21:20:43 +0900

    Design(todo item): 깔끔하게 보이게 정리

--- src/components/Todo/Item.vue
+++ src/components/Todo/Item.vue
@@ -24,3 +24,27 @@ watch(draft, (newValue) => {
     <input type="text" v-model="draft.title" />
   </div>
 </template>
+
+<style scoped>
+.todo_item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+}
+
+input[type='checkbox'] {
+  width: 30px;
+  height: 30px;
+
+  margin-right: 5px;
+}
+input[type='text'] {
+  flex-grow: 1;
+
+  padding: 5px 10px;
+
+  font-size: 16px;
+}
+</style>
